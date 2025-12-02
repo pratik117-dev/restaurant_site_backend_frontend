30c7220ee82e9bc5895096e74db4c6803201aa0e
pratik117-dev <pratikpoudel1110@gmail.com>
2025-12-02 16:41:17 +0545

    changed

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const AboutPage = () => {
   const [activeStory, setActiveStory] = useState(0);
-  const navigate = useNavigate();
+
 
   const stories = [
     {
